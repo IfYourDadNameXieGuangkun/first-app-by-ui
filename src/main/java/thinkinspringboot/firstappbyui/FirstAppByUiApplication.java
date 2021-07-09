460faac5aa6b3a303f98e0bc11e4164541d4d33c
白伟 <m15677360304@163.com>
2021-07-09 10:33:50 +0800

    Initial commit

---
+++ src/main/java/thinkinspringboot/firstappbyui/FirstAppByUiApplication.java
@@ -0,0 +1,13 @@
+package thinkinspringboot.firstappbyui;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstAppByUiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstAppByUiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/thinkinspringboot/firstappbyui/FirstAppByUiApplicationTests.java
@@ -0,0 +1,13 @@
+package thinkinspringboot.firstappbyui;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstAppByUiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
