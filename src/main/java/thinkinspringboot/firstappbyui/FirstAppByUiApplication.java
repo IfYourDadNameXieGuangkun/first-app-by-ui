927a04cf15ef3feaa4f76f98c13bc796205cd768
白伟 <m15677360304@163.com>
2021-07-13 18:34:45 +0800

    webflux小试牛刀

--- src/main/java/thinkinspringboot/firstappbyui/FirstAppByUiApplication.java
+++ src/main/java/thinkinspringboot/firstappbyui/FirstAppByUiApplication.java
@@ -1,7 +1,11 @@
 package thinkinspringboot.firstappbyui;
 
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.context.WebApplicationContext;
 
 @SpringBootApplication
 public class FirstAppByUiApplication {
